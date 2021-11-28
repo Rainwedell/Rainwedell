da489264c3cb1ea3f1dfeb3bc6dc3a7167c27a6c
Rain <li1153416007@outlook.com>
2021-11-28 13:11:05 +0800

    动态路由

---
+++ appdemo/src/pages/detail.js
@@ -0,0 +1,26 @@
+// import { Button } from "antd";
+import React from "react";
+// import { Link } from "react-router-dom";
+import "./app.scss";
+import "antd/dist/antd.css";
+
+export default function Detail() {
+  return <div class="container">this is detail.</div>;
+}
+// export default class Detail extends React.Component {
+//   handleJump = () => {
+//     this.props.history.push("/login");
+//   };
+//   render() {
+//     return (
+//       <div>
+//         <h1>欢迎来到腾讯课程</h1>
+//         <Link to="/login">点击跳转到登录页面</Link>
+//         <br />
+//         <Link to="/home">点击跳转到home</Link>
+//         <br />
+//         <Button onClick={this.handleJump}>登陆跳转</Button>
+//       </div>
+//     );
+//   }
+// }
--- appdemo/src/router.js
+++ appdemo/src/router.js
@@ -1,15 +1,26 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Login from "./pages/login";
 import Home from "./pages/home";
 import App from "./pages/app";
+import Detail from "./pages/detail";
+import NoMatch from "./pages/404";
 export default function IRouter() {
   return (
     <Router>
       <Routes>
         <Route exact path="/" element={<App />}></Route>
         <Route exact path="/login" element={<Login />}></Route>
-        <Route exact path="/home" element={<Home />}></Route>111
+        <Route exact path="/home" element={<Home />}>
+          <Route path="*" element={<Navigate to="/login" />} />
+        </Route>
+        <Route exact path="/detail/:id" element={<Detail />}></Route>
+        <Route path="*" element={<NoMatch />}></Route>
       </Routes>
     </Router>
   );
